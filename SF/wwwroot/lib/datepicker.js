e0fde4e2ea3188e0bc63c13348339903d2d74bd4
james-sparc <james@sparcsystems.africa>
2022-03-08 04:43:31 +0200

    net 6

---
+++ SF/wwwroot/lib/datepicker.js
@@ -0,0 +1,86 @@
+﻿function init_daterangepicker() {
+
+    if (typeof ($.fn.daterangepicker) === 'undefined') {
+        return;
+    }
+    console.log('init_daterangepicker');
+
+    var cb = function (start, end, label) {
+        //console.log(start.toISOString(), end.toISOString(), label);
+        $('#reportranges span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+
+        startDate = start.format('DD-MM-YYYY');
+        endDate = end.format('DD-MM-YYYY');
+
+        $("#startDate").val(startDate);
+        $("#endDate").val(endDate);
+
+
+        $("#start").val(startDate);
+        $("#end").val(endDate);
+
+        //console.log(startDate);
+    };
+
+    var optionSet1 = {
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2050',
+        dateLimit: {
+            days: 60
+        },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'left',
+        buttonClasses: ['btn btn-default'],
+        applyClass: 'btn-small btn-primary',
+        cancelClass: 'btn-small',
+        format: 'MM/DD/YYYY',
+        separator: ' to ',
+        locale: {
+            applyLabel: 'Submit',
+            cancelLabel: 'Clear',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+    };
+
+    $('#reportranges span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+    $('#reportranges').daterangepicker(optionSet1, cb);
+
+    $('#reportranges').on('apply.daterangepicker',
+        function (ev, picker) {
+            startDate = picker.startDate.format('DD-MM-YYYY');
+            endDate = picker.endDate.format('DD-MM-YYYY');
+
+            $("#startDate").val(startDate);
+            $("#endDate").val(endDate);
+        });
+
+    $('#options1').click(function () {
+        $('#reportranges').data('daterangepicker').setOptions(optionSet1, cb);
+    });
+    $('#options2').click(function () {
+        $('#reportranges').data('daterangepicker').setOptions(optionSet2, cb);
+    });
+    $('#destroy').click(function () {
+        $('#reportranges').data('daterangepicker').remove();
+    });
+
+}
---
+++ SF/wwwroot/lib/intl/build/js/intlScript.js
@@ -0,0 +1,52 @@
+﻿var input = document.querySelector("#phone");
+intlTelInput(input, {
+    initialCountry: "auto",
+    preferredCountries: ["mw", "rw", "zm"],
+            // separateDialCode: false,
+    geoIpLookup: function (success, failure) {
+        $.get("https://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            success(countryCode);
+        });
+    },
+    utilsScript: "/lib/intl/build/js/utils.js"
+        });
+
+
+var input = document.querySelector("#phone"),
+    errorMsg = document.querySelector("#error-msg"),
+    validMsg = document.querySelector("#valid-msg");
+
+// Error messages based on the code returned from getValidationError
+var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number", "Invalid number"];
+
+// Initialise plugin
+var intl = window.intlTelInput(input, {
+    utilsScript: "/lib/intl/build/js/utils.js"
+});
+
+var reset = function () {
+    input.classList.remove("error");
+    errorMsg.innerHTML = "";
+    errorMsg.classList.add("hide");
+    validMsg.classList.add("hide");
+};
+
+// Validate on blur event
+input.addEventListener('blur', function () {
+    reset();
+    if (input.value.trim()) {
+        if (intl.isValidNumber()) {
+            validMsg.classList.remove("hide");
+        } else {
+            input.classList.add("error");
+            var errorCode = intl.getValidationError();
+            errorMsg.innerHTML = errorMap[errorCode];
+            errorMsg.classList.remove("hide");
+        }
+    }
+});
+
+// Reset on keyup/change event
+input.addEventListener('change', reset);
+input.addEventListener('keyup', reset);
